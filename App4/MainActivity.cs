ced2ffdb86398817414d798c524487fe721fb60d
lucasmsantoswhirpool <lucas_m_santos@whirlpool.com>
2017-05-22 09:47:17 -0300

    APP finalizado

--- App4/MainActivity.cs
+++ App4/MainActivity.cs
@@ -14,10 +14,10 @@ using System.Collections.Generic;
 using System.Globalization;
 using Android.Graphics;
 using Android.Content.Res;
-
+using App4.Dialogs;
 
 namespace App4
-    {
+{
     [Activity(Label = "App4", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
@@ -27,13 +27,19 @@ namespace App4
         RestRequest cpf { get; set; }
         public RestClient consumer { get; set; }
         IRestResponse mensagemConsumer;
+        TextView txtsobrenome;
         RestClient orderId { get; set; }
         RestRequest requestorderId { get; set; }
         IRestResponse answerorder { get; set; }
         TextView txtnome;
+        TextView txtresp0;
+        TextView txtresp1;
+        TextView txtresp2;
+        TextView txtresp3;
+        TextView txtresp4;
         public List<string> lista;
         ListView mlistview;
-        
+       // ImageView imageView;
 
         protected override void OnCreate(Bundle bundle)
         {
@@ -46,21 +52,20 @@ namespace App4
             txtnome = FindViewById<TextView>(Resource.Id.txtNome);
             edtcpf = FindViewById<EditText>(Resource.Id.edtcpf);
             btnConsumer = FindViewById<Button>(Resource.Id.btnConsumer);
+            //imageView = FindViewById<ImageView>(Resource.Id.imageView1);
             btnConsumer.Click += BtnConsumer_Click;
             lista = new List<string>();
-           
         }
 
         private void BtnConsumer_Click(object sender, EventArgs e)
         {
-            
-
-                // API Consumer CPF
-                
             try
             {
-              
-                
+
+                // API Consumer CPF
+
+                imageView.SetImageResource(Resource.Drawable.giphy);
+
                 consumer = new RestClient("https://qa.api-latam.whirlpool.com/v1.0/consumers");
                 cpf = new RestRequest("/" + edtcpf.Text, Method.GET);
                 cpf.AddHeader("Content-Type", "application/json; charset=utf-8");
@@ -68,9 +73,9 @@ namespace App4
                 mensagemConsumer = consumer.Execute(cpf);
                 Pessoa pessoa = JsonConvert.DeserializeObject<Pessoa>(mensagemConsumer.Content);
                 txtnome.Text = "Nome: " + pessoa.firstName + " " + pessoa.lastName;
-                
 
-                    // API Consumer service-orders
+
+                // API Consumer service-orders
                 orderId = new RestClient("https://qa.api-latam.whirlpool.com/v1.0/consumers/");
                 requestorderId = new RestRequest("/" + edtcpf.Text + "/service-orders", Method.GET);
                 requestorderId.AddHeader("Content-Type", "application/json; charset=utf-8");
@@ -82,14 +87,12 @@ namespace App4
                 for (var i = 0; i < requestToken.orders.Count; i++)
                 {
                     end = requestToken.orders[i].order.orderId + " " + "StatusCode" + " - " + requestToken.orders[i].order.orderStatusCode + " "
-                   + "Status Description: " + " - " + requestToken.orders[i].order.orderStatusDescription + " "
-                   + "Data de criação de ordem: " + " - " + requestToken.orders[i].order.orderOpeningDate;
+                   + "Status Description: " + " - " + requestToken.orders[i].order.orderStatusDescription;
                     lista.Add(end);
                     ArrayAdapter<string> adapter = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleListItem1, lista);
                     mlistview.Adapter = adapter;
                 }
             }
-
 
             //        if (requestToken.orders[0].order.orderStatusCode == "CANC")
             //        {
